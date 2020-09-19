bfa237087d404665eda50f223bfaee5bf6a52c5c
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2020-09-19 13:48:21 +0900

    Done
    Done

---
+++ video.py
@@ -0,0 +1,17 @@
+from pytube import YouTube
+
+yt = YouTube(str(input("Enter the video link: ")))
+videos = yt.streams.all()
+
+s = 1
+for v in videos:
+    print(str(s)+". "+str(v))
+    s += 1
+
+n = int(input("Enter the number of the video: "))
+vid = videos[n-1]
+
+destination = str(input("Enter the destination: "))
+vid.download(destination)
+
+print("\nHas been successfully downloaded")
